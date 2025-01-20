257d5fc44812ba4fb3183134a18edf5027ecd689
khrousseau <khrousseau.art@gmail.com>
2025-01-20 18:24:03 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 (function($) {
-	var $body = $('body');
+    var $body = $('body');
 
-	// Play initial animations on page load.
-	$(window).on('load', function() {
-		setTimeout(function() {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
+    // Play initial animations on page load.
+    $(window).on('load', function() {
+        setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
+
+    // Add slide-up effect when "See Through My Eyes" is clicked
+    $('#header .content a').on('click', function(e) {
+        e.preventDefault(); // Prevent the link from navigating
+
+        // Add 'is-active' class to header to slide it up
+        $('#header').addClass('is-active');
+    });
 })(jQuery);
