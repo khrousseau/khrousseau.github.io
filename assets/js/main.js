312d6dc83d283072be7587ca2c038fc04d9349ef
khrousseau <khrousseau.art@gmail.com>
2025-01-21 20:40:23 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,22 +14,8 @@
         if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON') {
             e.preventDefault();
         }
-
-        // Get the height of the header dynamically
-        var headerHeight = $header.outerHeight();
-        
-        // Calculate the maximum translation value for the header to slide up without crossing the top border
-        var maxTranslateY = headerHeight > $(window).height() ? $(window).height() : headerHeight;
-
-        // Add the necessary classes for activation
         $header.addClass('is-active clicked');
         $header.addClass('fade-out');  // Add the fade-out class to the header
-
-        // Apply the calculated translateY value for smooth animation
-        $header.css('transform', 'translateY(-' + maxTranslateY + 'px)'); // Adjust the header's translation to fit within the viewport
-
-        // To trigger the background image fade-out effect, toggle a class
-        $header.addClass('background-fade');
     });
 
     $button.on('click', function(e) {
